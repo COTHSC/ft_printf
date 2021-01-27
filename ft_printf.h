a23fce396ff1b4d5fca2bc3763da5fb50d74e9a2
Jean SCULLY <jescully@e2r10p20.42.fr>
2021-01-27 10:36:47 +0100

    aded files

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: jescully <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/25 09:47:48 by jescully          #+#    #+#             */
-/*   Updated: 2021/01/26 12:06:44 by jescully         ###   ########.fr       */
+/*   Updated: 2021/01/27 10:36:34 by jescully         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,4 +38,5 @@ char    		*ft_appendchar(char const *s1, char const s2);
 char    		*ft_strnchr(const char *s, int c, int range);
 char        	*ft_Xtoa(unsigned int n);
 char        	*ft_xtoa(unsigned int n);
+char            *ft_chartostr(char c);
 #endif
